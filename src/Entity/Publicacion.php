a0bda2b0879e921b059c8019be897e104bd2b3ff
Juan03m <juanmazu03@gmail.com>
2024-05-09 10:58:25 -0300

    mas cambios a publicacion

--- src/Entity/Publicacion.php
+++ src/Entity/Publicacion.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PublicacionRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PublicacionRepository::class)]
@@ -19,6 +20,12 @@ class Publicacion
 
     #[ORM\Column(length: 255)]
     private ?string $titulo = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $descripcion = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fecha = null;
 
     public function getId(): ?int
     {
@@ -45,6 +52,30 @@ class Publicacion
     public function setTitulo(string $titulo): static
     {
         $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): static
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): static
+    {
+        $this->fecha = $fecha;
 
         return $this;
     }
