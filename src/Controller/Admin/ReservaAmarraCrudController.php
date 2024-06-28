762f114599a7ec0785762ef18f1dca2924292946
tobinapoli <tobiasnapoli15@gmail.com>
2024-06-28 18:30:53 -0300

    "cambio or por and"

--- src/Controller/Admin/ReservaAmarraCrudController.php
+++ src/Controller/Admin/ReservaAmarraCrudController.php
@@ -142,10 +142,10 @@ class ReservaAmarraCrudController extends AbstractCrudController
     $qb->select('entity')
         ->from($entityDto->getFqcn(), 'entity')
         ->andWhere(
-            $qb->expr()->orX(
+            $qb->expr()->andX(
                 $qb->expr()->eq('entity.fechaDesde', ':today'),
                 $qb->expr()->gt('entity.fechaDesde', ':today'),
-                $qb->expr()->lt('entity.fechaHasta', ':today')
+                $qb->expr()->lt('entity.fechaHasta', ':today'),
             )
         )
         ->setParameter('today', $today->format('Y-m-d'));
